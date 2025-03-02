491d1821cce8e0d5b10d786d523ad97add35356e
Hande Eylül Memikoğlu <handememikoglu@gmail.com>
2025-03-02 12:48:50 +0300

    edit fonksiyonu eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [page, setPage] = useState("index");
   const[toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
+  const [editToDo, setEditToDo] = useState(null);
 
   const handleChange =(e) =>{
     setToDo(e.target.value);
@@ -22,6 +23,22 @@ function App() {
   const deleteTask = (id) =>{
     const filterTask = toDos.filter((todo) => todo.id !== id);
     setToDos(filterTask);
+  }
+
+  const editTask = (todo) => {
+    setEditToDo(todo);
+    setPage("editPage");
+  }
+
+  const handleUpdate = () => {
+    setToDos(
+      toDos.map(todo => todo.id === editToDo.id ? {...todo, text: editToDo.text} : todo)
+    )
+    setEditToDo("");
+    setPage("index");
+  }
+  const handleCancel = () =>  {
+    setPage("index");
   }
 
   return (
@@ -40,7 +57,7 @@ function App() {
                     <li key={todo.id} className="list-ToDo">
                       {todo.text} 
                       <div className="list-btn-container">
-                        <button><i className="fa-solid fa-pencil"></i></button>
+                        <button onClick={() => editTask(todo)}><i className="fa-solid fa-pencil"></i></button>
                         <button onClick={() => deleteTask(todo.id)}><i className="fa-solid fa-trash-can"></i></button>
                         <button><i className="fa-solid fa-circle-check"></i></button>
                       </div>
@@ -64,6 +81,21 @@ function App() {
               <input type="text" placeholder='Title...' name='toDo' value={toDo} onChange={handleChange}/>
               <button onClick={handleAdd}>ADD</button>
             </div>
+          )
+        }
+        {
+          page === "editPage" && (
+            <>
+              <div className='edit-page-container'>
+                <input type="text" placeholder='Edit Title...'
+                value={editToDo.text}
+                onChange={(e) => setEditToDo({...editToDo, text: e.target.value})}/>
+                <div className="edit-page-btns">
+                  <button onClick={handleUpdate}>Update</button>
+                  <button onClick={handleCancel}>Cancel</button>
+                </div>
+              </div>
+            </>
           )
         }
       </div>
